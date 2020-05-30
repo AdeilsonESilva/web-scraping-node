7cc85aa62835b72f929da30e9a762b67f8444d15
Adeilson Silva <adeilson@gmail.com>
2020-05-30 11:09:08 -0300

    Validation invalid values

--- src/index.js
+++ src/index.js
@@ -11,27 +11,35 @@ console.log("Welcome to currency conversion Bot 💰");
 const robot = async () => {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
+  const value = readlineSync.question("Type value to convert (1): ") || "1";
   const currencyFrom =
     readlineSync.question("Type currency from (dolar): ") || "dolar";
   const currencyTo =
     readlineSync.question("Type currency to (real): ") || "real";
-  const url = `https://www.google.com/search?q=${currencyFrom}+para+${currencyTo}`;
+  const url = `https://www.google.com/search?q=${value}+${currencyFrom}+em+${currencyTo}`;
 
   await page.goto(url);
   // await page.screenshot({ path: "example.png" });
 
-  const result = await page.evaluate(
-    () =>
-      document.querySelector(
+  const result = await page.evaluate(() => {
+    try {
+      return document.querySelector(
         "#knowledge-currency__updatable-data-column > div.H07hi > table > tbody > tr:nth-child(3) > td:nth-child(1) > input"
-      ).value
-  );
-
-  console.log(
-    `The value of the 1 ${currencyFrom} in ${currencyTo} is: ${result}`
-  );
+      ).value;
+    } catch (error) {
+      return undefined;
+    }
+  });
 
   await browser.close();
+
+  if (!result) {
+    return console.log("It was not possible to convert the value");
+  }
+
+  console.log(
+    `The value of the ${value} ${currencyFrom} in ${currencyTo} is: ${result}`
+  );
 };
 
 robot();
